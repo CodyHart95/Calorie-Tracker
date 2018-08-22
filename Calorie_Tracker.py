86d677e0202bff2451ededd5c42f3d88dcd50503
Cody Hart <cody@hartshooting.com>
2018-08-21 20:09:11 -0500

    added if statement to preform actions with commands

--- Calorie_Tracker.py
+++ Calorie_Tracker.py
@@ -1,28 +1,38 @@
 
 class Calorie_Tracker(object):
-    def __init__(self):
-        self.getCommand()
+    user = ''
+    def __init__(self, user):
+        self.getCommand(user)
         
     #provides an interface so that users may enter commands
-    def getCommand(self):
+    def getCommand(self, user):
         while True:
             print(50 * "-")
             command = input("\nPlease enter a command: ".lower())
             print()
-    def addCalories(self,new_calories):
-        p = 0
-
-    def displayTodaysCalories(self,date):
-        p = 0
-
+            if(command == "--a"):
+                self.addCalories()
+            elif(command == "--t"):
+                self.displayTodaysCalories()
+            elif(command == "--w"):
+                self.displayUsersCurrentWeight(user)
+            elif(command == "--h"):
+                self.commandHelper()
+            else:
+                print("I dont recognize that command. Here is a list of my commands/n")
+                self.commandHelper()
+    def addCalories(self):
+        print("cal")
+    def displayTodaysCalories(self):
+        print("today")
     def getUserInformation(self, user_name):
         p = 0
 
     def displayUsersCurrentWeight(self, user_name):
-        p = 0
-
+        print("weight")
     def updateUserInformation(self, user_name):
         p = 0
-        
+        commandHelper(self):
+            print("help")
 if __name__ == "__main__":
-    Calorie_Tracker()
+    Calorie_Tracker("Cody")
