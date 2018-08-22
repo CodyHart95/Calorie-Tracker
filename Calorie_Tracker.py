3413ea087a7dd7e97fa3c46865bcc5d28ea934f3
Cody Hart <cody@hartshooting.com>
2018-08-22 07:38:10 -0500

    added updateCalories function to User class. Currently receiving an invalid token error when reading in xml files.

--- Calorie_Tracker.py
+++ Calorie_Tracker.py
@@ -1,39 +1,33 @@
 from User import User
+
 class Calorie_Tracker(object):
-    user_name = ''
-    def __init__(self, user):
-        self.user_name = user
-        self.getCommand()
-        
+    def __init__(self, user):       
+        current_user = User(user)
+        self.getCommand(current_user)
+    
+    
+    
     #provides an interface so that users may enter commands
-    def getCommand(self):
-        while True:
-            current_user = User(self.user_name)
+    def getCommand(self,current_user):
+        while True:            
             print(50 * "-")
             command = input("\nPlease enter a command: ").lower()
             print()
             if(command == "--a"):
-                self.addCalories()
+                self.addCalories(current_user)
             elif(command == "--t"):
-                self.displayTodaysCalories()
+                print("You have consumed " + current_user.todays_calories + " calories today!")
             elif(command == "--w"):
-                self.displayUsersCurrentWeight(user)
+                print("Based on your current calorie intake you should weigh " + str(current_user.weight)+ "lbs.")
             elif(command == "--help"):
                 self.commandHelper()
             else:
                 print("I dont recognize that command. Here is a list of my commands:\n")
                 self.commandHelper()
-    def addCalories(self):
-        print("cal")
-    def displayTodaysCalories(self):
-        print("today")
-    def getUserInformation(self, user_name):
-        p = 0
+    def addCalories(self,current_user):
+        cals = input("Please enter the calories you have eaten since your last log in: ")
+        current_user.updateCalories(int(cals))
 
-    def displayUsersCurrentWeight(self, user_name):
-        print("weight")
-    def updateUserInformation(self, user_name):
-        p = 0
     def commandHelper(self):
          print(50 * "-")
          print("--a: Opens a prompt to allow you to add calories to your total for today.\n"
--- User.py
+++ User.py
@@ -1,29 +1,27 @@
 import os.path
 from os import getcwd
 import xml.etree.ElementTree as ET
+import xml.etree
 from xml.etree.ElementTree import ElementTree
 from datetime import date
+
 class User(object):
     name = ''
     todays_calories = 0
     weight = 0
+    path = ''
     def __init__(self,user):
         self.name = user
+        self.path = os.path.join(getcwd(),('user_files/' + self.name + '.xml'))
         self.readUserFile()
         
-    #def setTodaysCalories:
-    #def getTodaysCalories:
-    #def setWeight:
-        
-    #def getWeight:
-    def readUserFile(self):
-        path = os.path.join(getcwd(),('user_files/' + self.name + '.xml'))
-        if(os.path.isfile(path)):
-            tree = ET.parse(path)
+    def readUserFile(self):       
+        if(os.path.isfile(self.path)):
+            tree = ET.parse(self.path, etree.XMLParser(encoding='utf-8'))
             root = tree.getroot()
             calorie_counts = root.find('calorie_counts')
-            self.setTodaysCalories(calorie_counts.find(date.today()))
-            self.setWeight(root.find(weight).text)
+            self.todays_calories = int(calorie_counts.find(str(date.today())).text)
+            self.weight = int(root.find(weight).text)
         else:
             self.createUserFile()
                         
@@ -33,7 +31,20 @@ class User(object):
         tree = ElementTree(root)
         user_name = ET.SubElement(root,'name')
         user_name.text = self.name
-        user_weight = ET.SubElement(root,'wieght')
+        user_weight = ET.SubElement(root,'weight')
         user_weight.text = current_weight
         calorie_counts = ET.SubElement(root,'calorie_counts')
-        tree.write(os.path.join(getcwd(),('user_files/' + self.name + '.xml')))
+        todays_calories = ET.SubElement(calorie_counts,str(date.today()))
+        todays_calories.text = '0'
+        tree.write(self.path)
+        
+    def updateCalories(self,new_calories):
+        self.todays_calories = self.todays_calories + new_calories
+        tree = ET.parse(self.path)
+        root = tree.getroot()
+        calorie_counts = root.find('calorie_counts')
+        current_calories = calorie_counts.find(str(date.today()))
+        current_calories.text = self.todays_calories
+        new_tree = ElementTree(root)
+        new_tree.write(self.path)
+        
