2a7e3346683f6d6f05bcee0c27a7ee68b38e924a
Cody Hart <cody@hartshooting.com>
2018-08-21 21:33:18 -0500

    added functionality to readUserFile and createUserFile in User class

--- User.py
+++ User.py
@@ -1,3 +1,7 @@
+import os.path
+from os import getcwd
+import xml.etree.ElementTree as ET
+from datetime import date
 class User(object):
     name = ''
     todays_calories = 0
@@ -9,6 +13,23 @@ class User(object):
     def getTodaysCalories:
     def setWeight:
     def getWeight:
-    def readUserFile:
-    def createUserFile:
-    
+    def readUserFile(self):
+        path = os.path.join(getcwd(),('user_files/' + self.user + '.xml'))
+        if(os.path.isfile(path)):
+            tree = ET.parse(path)
+            root = tree.getroot()
+            self.setUserName(root.find('name').text)
+            calorie_counts = root.find('calorie_counts')
+            self.setTodaysCalories(calorie_counts.find(date.today())
+            self.setWeight(root.find(weight).text)
+        else:
+            self.createUserFile(self.name)
+                        
+    def createUserFile(self):
+        current_weight = 0
+        root = ET.Element('root')
+        user_name = ET.SubElement('name')
+        user_name.text = self.name
+        user_weight = ET.SubElement('wieght')
+        user_weight.text = str(current_weight)
+        calorie_counts = ET.SubElement('calorie_counts')
