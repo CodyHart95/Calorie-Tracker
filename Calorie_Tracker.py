ef1dbe8aeb779f5a2b6802d91f89cd1eced1cb00
Cody Hart <cody@hartshooting.com>
2018-08-21 19:55:51 -0500

    added place holder functions for future use

--- Calorie_Tracker.py
+++ Calorie_Tracker.py
@@ -9,6 +9,20 @@ class Calorie_Tracker(object):
             print(50 * "-")
             command = input("\nPlease enter a command: ".lower())
             print()
+    def addCalories(self,new_calories):
+        p = 0
+
+    def displayTodaysCalories(self,date):
+        p = 0
+
+    def getUserInformation(self, user_name):
+        p = 0
+
+    def displayUsersCurrentWeight(self, user_name):
+        p = 0
+
+    def updateUserInformation(self, user_name):
+        p = 0
         
 if __name__ == "__main__":
     Calorie_Tracker()
