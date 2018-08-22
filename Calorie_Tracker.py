52061c8816d7edd332c75da3753c8e205278cfb1
Cody Hart <cody@hartshooting.com>
2018-08-21 19:48:58 -0500

    added basic structure for user command interface

--- Calorie_Tracker.py
+++ Calorie_Tracker.py
@@ -1,7 +1,14 @@
 
 class Calorie_Tracker(object):
     def __init__(self):
+        self.getCommand()
         
+    #provides an interface so that users may enter commands
+    def getCommand(self):
+        while True:
+            print(50 * "-")
+            command = input("\nPlease enter a command: ".lower())
+            print()
         
 if __name__ == "__main__":
     Calorie_Tracker()
